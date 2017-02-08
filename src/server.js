a1b556e6a9d9446b4c904ca215be7490f40b3bba
Jordan Mato <jordo754@gmail.com>
2017-02-08 15:49:59 -0500

    continued work

--- src/server.js
+++ src/server.js
@@ -26,8 +26,57 @@ const onJoined = (sock) => {
   const socket = sock;
 
   socket.on('join', (data) => {
+    // check if user is already in room
+    if (users[data.name]) {
+      const joinMsg = {
+        name: 'server',
+        msg: `Name '${data.name}' is already in use. Please select a new name.`,
+      };
+
+      socket.name = data.name;
+      socket.emit('msg', joinMsg);
+    } 
+    else {
+      // message back to new user
+      const joinMsg = {
+        name: 'server',
+        msg: `There are ${Object.keys(users).length} users online`,
+      };
+
+      socket.name = data.name;
+      socket.emit('msg', joinMsg);
+
+      socket.join('room1');
+
+      // announcement to everyone in room
+      const response = {
+        name: 'server',
+        msg: `${data.name} has joined the room.`,
+      };
+      socket.broadcast.to('room1').emit('msg', response);
+
+      console.log(`${data.name} joined`);
+      // success message back to new user
+      socket.emit('msg', { name: 'server', msg: 'You joined the room' });
+      users[data.name] = socket;
+    }
+  });
+};
+
+const onMsg = (sock) => {
+  const socket = sock;
+
+  socket.on('msgToServer', (data) => {
+    io.sockets.in('room1').emit('msg', { name: socket.name, msg: data.msg });
+  });
+};
+
+const onDisconnect = (sock) => {
+  const socket = sock;
+  
+  socket.on('disconnect', (data) => {
     // message back to new user
-    const joinMsg = {
+    const leaveMsg = {
       name: 'server',
       msg: `There are ${Object.keys(users).length} users online`,
     };
@@ -47,19 +96,8 @@ const onJoined = (sock) => {
     console.log(`${data.name} joined`);
     // success message back to new user
     socket.emit('msg', { name: 'server', msg: 'You joined the room' });
+    users[data.name] = socket;
   });
-};
-
-const onMsg = (sock) => {
-  const socket = sock;
-
-  socket.on('msgToServer', (data) => {
-    io.sockets.in('room1').emit('msg', { name: socket.name, msg: data.msg });
-  });
-};
-
-const onDisconnect = (sock) => {
-  const socket = sock;
 };
 
 io.sockets.on('connection', (socket) => {
