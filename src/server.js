effb9a5e8e16a6e8a1ccd4e4ebe544af0397d550
Jordan <jordo754@gmail.com>
2017-02-09 02:05:04 -0500

    Added colors, pm, chat commands, sending messages, etc

--- src/server.js
+++ src/server.js
@@ -21,6 +21,8 @@ const io = socketio(app);
 
 // object to hold all of our connected users
 const users = {};
+const userColors = ['red', 'blue', 'green', 'orange', 'purple'];
+let count = 0;
 
 const onJoined = (sock) => {
   const socket = sock;
@@ -29,34 +31,46 @@ const onJoined = (sock) => {
     // check if user is already in room
     if (users[data.name]) {
       const joinMsg = {
-        name: 'server',
+        name: 'Server',
         msg: `Name '${data.name}' is already in use. Please select a new name.`,
       };
 
-      socket.name = data.name;
-      socket.emit('msg', joinMsg);
+      socket.emit('nameError', joinMsg);
+    } else if (data.name === 'unknown') {
+      const joinMsg = {
+        name: 'Server',
+        msg: 'Please enter a name.',
+      };
+
+      socket.emit('nameError', joinMsg);
     } else {
       // message back to new user
       const joinMsg = {
-        name: 'server',
+        name: 'Server',
         msg: `There are ${Object.keys(users).length} users online`,
       };
 
       socket.name = data.name;
+      socket.color = userColors[count];
+      count++;
+      if (count > userColors.size) {
+        count = 0;
+      }
       socket.emit('msg', joinMsg);
 
       socket.join('room1');
 
       // announcement to everyone in room
       const response = {
-        name: 'server',
-        msg: `${data.name} has joined the room.`,
+        name: 'Server',
+        msg: `/join ${data.name} has joined the room.`,
+        color: socket.color,
       };
       socket.broadcast.to('room1').emit('msg', response);
 
       console.log(`${data.name} joined`);
       // success message back to new user
-      socket.emit('msg', { name: 'server', msg: 'You joined the room' });
+      socket.emit('msg', { name: 'Server', msg: 'You joined the room' });
       users[data.name] = socket;
     }
   });
@@ -64,9 +78,37 @@ const onJoined = (sock) => {
 
 const onMsg = (sock) => {
   const socket = sock;
-
   socket.on('msgToServer', (data) => {
-    io.sockets.in('room1').emit('msg', { name: socket.name, msg: data.msg });
+    if (data.msg[0] === '/') {
+      const commandString = data.msg.split(' ')[0];
+      const messageString = data.msg.substring(data.msg.indexOf(' ') + 1);
+
+      switch (commandString) {
+        case '/w': {
+          const user = messageString.split(' ')[0];
+          const pm = messageString.substring(messageString.indexOf(' ') + 1);
+          if (users[user]) {
+            // send back to user
+            socket.emit('msg', { name: socket.name, msg: `${commandString} To ${user} ${pm}`, color: socket.color, recipColor: users[user].color });
+
+            // send to recipient
+            users[user].emit('msg', { name: socket.name, msg: `${commandString} From ${socket.name} ${pm}`, color: users[user].color, senderColor: socket.color });
+          }
+          break;
+        }
+        case '/join': {
+          break;
+        }
+        default: {
+          if (socket.name) {
+            socket.emit('msg', { name: socket.name, msg: data.msg, color: socket.color });
+          }
+          break;
+        }
+      }
+    } else {
+      io.sockets.in('room1').emit('msg', { name: socket.name, msg: data.msg, color: socket.color });
+    }
   });
 };
 
@@ -75,16 +117,19 @@ const onDisconnect = (sock) => {
 
   socket.on('disconnect', () => {
     // announcement to everyone in room
-    const response = {
-      name: 'server',
-      msg: `${socket.name} has left the room.`,
-    };
-    socket.broadcast.to('room1').emit('msg', response);
+    if (socket.name) {
+      const response = {
+        name: 'Server',
+        msg: `/leave ${socket.name} has left the room.`,
+        color: socket.color,
+      };
+      socket.broadcast.to('room1').emit('msg', response);
 
-    console.log(`${socket.name} left`);
-    // success message back to new user
-    socket.emit('msg', { name: 'server', msg: 'You left the room' });
-    delete users[socket.name];
+      console.log(`${socket.name} left`);
+      // success message back to new user
+      socket.emit('msg', { name: 'Server', msg: 'You left the room' });
+      delete users[socket.name];
+    }
   });
 };
 
